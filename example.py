73c62043ab0e56e4c60ad906d6697bb50ff9747a
lovenjak <klemen.lovenjak@gmail.com>
2017-12-30 16:02:09 +0100

    Minor bugfixes.

---
+++ example.py
@@ -0,0 +1,37 @@
+from utils import *
+
+
+def main():
+
+    # Using arso-lidar2dtm in Python
+
+    y = 567536
+    x = 142344
+    dx = 250
+    dy = 300
+
+    x_min = x - dx
+    x_max = x + dx
+    y_min = y - dy
+    y_max = y + dy
+
+    step = 15
+    output = "output"
+    start_index = 20000
+    workspace = os.path.abspath('C:/Users/Lovenjak/Desktop/test')
+
+    ks = "D48GK"  # or "D96TM"
+    x_range, y_range = pt2range(x, y, dx, dy)
+
+    # Initiate program.
+    dbf_file, fn_ks = initiate(ks, workspace)
+
+    # Download LIDAR data.
+    get_data(workspace, dbf_file, ks, fn_ks, x_range, y_range, x_min, y_min, x_max, y_max)
+
+    # Create grid file.
+    create_grid(workspace, output, step, start_index)
+
+
+if __name__ == "__main__":
+    main()
